2b3d6aaa9d4cffc2b716f4e0af86d0a55cfc2ca4
Uyooghene <uyooghene01@gmail.com>
2024-11-15 08:25:25 +0100

    middleware

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const app = express();
 const morgan = require('morgan'); 
 
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+app.use('/dogs',(req,res, next) =>{
+    console.log('/dogs middleware')
+    next();
+})
 
 app.get('/', (req, res) =>{
+    console.log(`request time : ${req.requestTime}`)
     res.send('home')
 })
 app.get('/dogs', (req, res) =>{
+    console.log(`request time : ${req.requestTime}`)
     res.send('Woof')
+})
+
+app.use((req, res)=>{
+    res.status(404).send('NOT found')
 })
 
 app.listen(3000, () =>{
